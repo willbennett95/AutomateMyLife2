2d038f6c8f13a7ce0ff36074bc869654b543d579
Will Bennett <wb00080@surrey.ac.uk>
2018-03-06 03:10:29 +0000

    added ability to post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,23 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
+
   def profile
-    
     # take the username as :id
     if(User.find_by_username(params[:id]))
       @username = params[:id]
     else
       redirect_to root_path, :notice => "User not found"
     end
-    
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id])) # only shows current user's posts on their profile
+    @newPost = Post.new 
   end
 
+
   def explore
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get '/user/:id' => 'pages#profile' # routes to the logged in user
   
   get '/explore' => 'pages#explore'
+  
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
